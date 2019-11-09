42133932274ab1505150a86b80098a1fb0983642
dannyvaks <56206612+dannyvaks@users.noreply.github.com>
2019-11-09 13:18:54 +0200

    home 091119
    update python folder

---
+++ python/.ipynb_checkpoints/ds_demo-checkpoint.py
@@ -0,0 +1,41 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "def checkLanguage(lang):\n",
+    "    if(lang == \"R\"):\n",
+    "        msg = \"We will use R extensively on machine learning!\"\n",
+    "    elif(lang == \"Python\"):\n",
+    "        msg = \"We will use Python extensively on machine learning and deep learning!\"\n",
+    "    else:\n",
+    "        msg = \"Sorry, we will not use %s on machine learning!\" % lang\n",
+    "    print(msg)"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.6.8"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
